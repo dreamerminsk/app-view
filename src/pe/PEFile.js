cf64a489a0f904c96b22ea800c84bc7194342f83
Micah Teeuws /dKCh/ <tutmirage@gmail.com>
2020-06-05 18:43:50 +0300

    Update PEFile.js

--- src/pe/PEFile.js
+++ src/pe/PEFile.js
@@ -1,3 +1,11 @@
 export default class PEFile {
+    
+    constructor(file) {
+        this.file = file;
+        this.headers = new Headers(this);
+    }
 
+    async readBuffer(fromByte, toByte) {
+        return readBuffer(this.file, 0, 2*4096);
+    }
 }
